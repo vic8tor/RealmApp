9f326d966e15d4df1843de2bb667dde0109e377c
vic8tor <timongrinni@gmail.com>
2022-01-29 16:53:32 +0300

    Added the Model

--- RealmApp/Models/TaskList.swift
+++ RealmApp/Models/TaskList.swift
@@ -5,4 +5,17 @@
 //  Created by Victor on 29.01.2022.
 //
 
-import Foundation
+import RealmSwift
+
+class TaskList: Object {
+    @Persisted var name = ""
+    @Persisted var date = Date()
+    @Persisted var tasks = List<Task>()
+}
+
+class Task: Object {
+    @Persisted var name = ""
+    @Persisted var note = ""
+    @Persisted var date = Date()
+    @Persisted var isComplete = false
+}
