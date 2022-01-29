9265f90fd8505d528dd7a3f58d9ce8edcf992471
vic8tor <timongrinni@gmail.com>
2022-01-29 13:47:21 +0300

    Added AlertController

--- RealmApp/ViewControllers/AlertController.swift
+++ RealmApp/ViewControllers/AlertController.swift
@@ -7,23 +7,62 @@
 
 import UIKit
 
-class AlertController: UIAlertController {
+import UIKit
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+extension UIAlertController {
+    
+    static func createAlert(withTitle title: String, andMessage message: String) -> UIAlertController {
+        UIAlertController(title: title, message: message, preferredStyle: .alert)
+    }
+        
+    func action(with taskList: TaskList?, completion: @escaping (String) -> Void) {
+        
+        let doneButton = taskList == nil ? "Save" : "Update"
+                
+        let saveAction = UIAlertAction(title: doneButton, style: .default) { _ in
+            guard let newValue = self.textFields?.first?.text else { return }
+            guard !newValue.isEmpty else { return }
+            completion(newValue)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        addAction(saveAction)
+        addAction(cancelAction)
+        addTextField { textField in
+            textField.placeholder = "List Name"
+            textField.text = taskList?.name
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func action(with task: Task?, completion: @escaping (String, String) -> Void) {
+                        
+        let title = task == nil ? "Save" : "Update"
+        
+        let saveAction = UIAlertAction(title: title, style: .default) { _ in
+            guard let newTask = self.textFields?.first?.text else { return }
+            guard !newTask.isEmpty else { return }
+            
+            if let note = self.textFields?.last?.text, !note.isEmpty {
+                completion(newTask, note)
+            } else {
+                completion(newTask, "")
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        addAction(saveAction)
+        addAction(cancelAction)
+        
+        addTextField { textField in
+            textField.placeholder = "New task"
+            textField.text = task?.name
+        }
+        
+        addTextField { textField in
+            textField.placeholder = "Note"
+            textField.text = task?.note
+        }
     }
-    */
-
 }
