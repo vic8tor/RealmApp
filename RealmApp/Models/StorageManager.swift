89c059fc65edca8e4e3e43ea2391b439cc624911
vic8tor <timongrinni@gmail.com>
2022-01-29 13:44:08 +0300

    Create structure of projects

---
+++ RealmApp/Models/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  RealmApp
+//
+//  Created by Victor on 29.01.2022.
+//
+
+import Foundation
---
+++ RealmApp/Models/TaskList.swift
@@ -0,0 +1,8 @@
+//
+//  TaskList.swift
+//  RealmApp
+//
+//  Created by Victor on 29.01.2022.
+//
+
+import Foundation
